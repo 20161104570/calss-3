0f99c55cb785f7fbaed1e00c4ffc5a1621a2d65c
20161104570 <a20161104570@ciecimac17deiMac.local>
2018-09-12 11:44:17 +0800

    1-100
    accumulation

---
+++ 1——100/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  1——100
+//
+//  Created by 20161104570 on 18/9/7.
+//  Copyright © 2018年 20161104570. All rights reserved.
+//
+
+import Foundation
+
+var a = 0
+
+for x in 1...100{
+    a = x + a
+}
+
+
+print(a)
+
